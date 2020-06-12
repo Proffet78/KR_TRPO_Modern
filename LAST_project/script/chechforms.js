df09f9e04759e3036fc07f8e75aa484a4a9f8b09
Proffet78 <valerimash78@gmail.com>
2020-06-12 18:36:16 +0300

    SPRINT1 comit2

---
+++ LAST_project/script/chechforms.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+function checkValue('fr1'){
+  	var input = document.getElementById('mail').value;
+    if (input ==""){
+		alert("Заполните все поля!");
+		return false;
+        }
+}
+	        
