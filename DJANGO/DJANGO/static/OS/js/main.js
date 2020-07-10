4118ccfb1fa92d1010e7f2c3a077b830bc15ce93
JennyShel18 <EvgeniaShelkunova00@gmail.com>
2020-07-11 00:43:59 +0400

    Регистрация пользователя на DJANGO version 1.3 #15

--- DJANGO/DJANGO/static/OS/js/main.js
+++ DJANGO/DJANGO/static/OS/js/main.js
@@ -71,3 +71,20 @@ jQuery(document).ready(function( $ ) {
 		}
 
 });
+
+function Registration() {
+	var form = document.getElementsByClassName("Ractivity")[0];
+	form.style="display: inline-block";
+}
+
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
