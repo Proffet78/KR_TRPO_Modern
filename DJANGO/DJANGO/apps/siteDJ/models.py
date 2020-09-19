344554deed8abc2c662aff9090d03a3e96b4941e
JennyShel18 <EvgeniaShelkunova00@gmail.com>
2020-09-20 01:25:26 +0400

    Исправлен файл gitignore, удалена вторая модель

--- DJANGO/DJANGO/apps/siteDJ/models.py
+++ DJANGO/DJANGO/apps/siteDJ/models.py
@@ -4,7 +4,3 @@
 class UserOne (models.Model):
   email = models.CharField(max_length=30)
   password = models.CharField(max_length=20)
-
-class UserTwo (models.Model):
-  email = models.CharField(max_length=30)
-  password = models.CharField(max_length=20)
